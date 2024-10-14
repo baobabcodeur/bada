2b565c55caf5a5441edf9bd9fdcd987952b04d90
Queen-Ora <jeanamekpod@gmail.com>
2024-10-14 14:39:22 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple alert for form submission (can be expanded)
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Merci de nous avoir contacté ! Nous vous répondrons bientôt.');
+});
